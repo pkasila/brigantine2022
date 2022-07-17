dc9ad1da5492ac6c39309f07c56012382bdeed49
Pavel Kasila <pavel.kasila@gmail.com>
2022-07-17 21:28:42 +0300

    Update dataset_shutter

--- dataset_shutter/main.py
+++ dataset_shutter/main.py
@@ -24,59 +24,66 @@ root = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.ID, "ro
 
 new_dataset = []
 
+
+def get_pos(elem):
+    x = elem.location['x']
+    y = elem.location['y']
+    w = elem.size['width']
+    h = root.size['height']
+    width = x + w
+    height = y + h
+
+    return {
+        "x": x,
+        "y": y,
+        "width": width,
+        "height": height
+    }
+
+
+def multiply_pos(pos, factor):
+    return {
+        "x": pos["x"] * factor,
+        "y": pos["y"] * factor,
+        "width": pos["width"] * factor,
+        "height": pos["height"] * factor
+    }
+
+
 for idx in range(0, dataset['total']):
     driver.execute_script('dispatchEvent(new Event("renderNewFormula"))')
     formula = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.ID, "formula")))
 
     driver.save_screenshot(f'{args.out}/{idx}.png')
 
-    location = root.location
-    size = root.size
-
-    x = location['x']
-    y = location['y']
-    w = size['width']
-    h = size['height']
-    width = x + w
-    height = y + h
-
-    root_pos = {
-        "x": x,
-        "y": y,
-        "width": width,
-        "height": height
-    }
-
-    location = formula.location
-    size = formula.size
-
-    x = location['x'] - root_pos['x']
-    y = location['y'] - root_pos['y']
-    w = size['width']
-    h = size['height']
-    width = x + w
-    height = y + h
-
-    formula_pos = {
-        "x": x,
-        "y": y,
-        "width": width,
-        "height": height
-    }
+    root_pos = get_pos(root)
+    formula_pos = get_pos(formula)
 
     im = Image.open(f'{args.out}/{idx}.png')
-    im = im.crop((int(x) * 2, int(y) * 2, int(width) * 2, int(height) * 2))
+    im = im.crop((int(root_pos["x"]) * 2,
+                  int(root_pos["y"]) * 2,
+                  int(root_pos["width"]) * 2,
+                  int(root_pos["height"]) * 2))
     im.save(f'{args.out}/{idx}.png')
+
+    formula_pos["width"] = formula_pos["width"] - root_pos["x"]
+    formula_pos["height"] = formula_pos["height"] - root_pos["y"]
+    formula_pos["x"] -= root_pos["x"]
+    formula_pos["y"] -= root_pos["y"]
+
+    root_pos["width"] = root_pos["width"] - root_pos["x"]
+    root_pos["height"] = root_pos["height"] - root_pos["y"]
+    root_pos["x"] = 0
+    root_pos["y"] = 0
 
     new_dataset.append({
         "file": f'{idx}.png',
-        "text": root_pos,
-        "formula": formula_pos
+        "text": multiply_pos(root_pos, 2),
+        "formula": multiply_pos(formula_pos, 2)
     })
     print(f"{idx}.png")
 
 with open(f'{args.out}/dataset.json', 'wb') as f:
     f.write(json.dumps(new_dataset, indent=2).encode('utf8'))
 
-driver.close()
 driver.quit()
